dc1befa6c836d91227f7220bdab65273d4e6a9f3
SocrateSkull <qoopefaax@gmail.com>
2024-09-10 23:03:33 -0300

    "actualización"

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CardGenerator from './components/CardGenerator';
 import AddressGenerator from './components/AddressGenerator';
 import './App.css';  // Asegúrate de que esta línea esté presente
@@ -8,13 +8,13 @@ const App = () => {
     return (
         <Router>
             <div className="App">
-                <Switch>
+                <Routes>
                     <Route path="/generate-cards" component={CardGenerator} />
                     <Route path="/generate-address" component={AddressGenerator} />
                     <Route path="/" exact>
                         <h1>Bienvenido a Dongato App</h1>
                     </Route>
-                </Switch>
+                </Routes>
             </div>
         </Router>
     );
