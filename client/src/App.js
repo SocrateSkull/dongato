6848e2f0190cca02a8bc8c236026531fed1adf4b
SocrateSkull <qoopefaax@gmail.com>
2024-09-11 21:47:28 -0300

    "verificando posible error"

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CardGenerator from './components/CardGenerator';
 import AddressGenerator from './components/AddressGenerator';
-import './App.css';  // Asegúrate de que esta línea esté presente
+import './App.css';  // Asegúrate de que este archivo exista
 
 const App = () => {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/generate-cards" component={CardGenerator} />
-                    <Route path="/generate-address" component={AddressGenerator} />
-                    <Route path="/" exact>
-                        <h1>Bienvenido a Dongato App</h1>
-                    </Route>
+                    {/* Ruta para generar tarjetas */}
+                    <Route path="/generate-cards" element={<CardGenerator />} />
+                    
+                    {/* Ruta para generar direcciones */}
+                    <Route path="/generate-address" element={<AddressGenerator />} />
+                    
+                    {/* Ruta principal */}
+                    <Route path="/" element={<h1>Bienvenido a Dongato App</h1>} />
                 </Routes>
             </div>
         </Router>
