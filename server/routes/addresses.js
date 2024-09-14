95e02b9942558aea7bd5681a1d04840bbf9b81ba
SocrateSkull <qoopefaax@gmail.com>
2024-09-14 15:14:46 -0300

    #eliminación y renovación de archivos"

--- server/routes/addresses.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateRandomAddress } = require('../controllers/addressesController');
-
-router.post('/generate', generateRandomAddress);
-
-module.exports = router;
--- server/routes/cards.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateCards, verifyCard, generateCustomCards } = require('../controllers/cardsController');
-
-router.post('/generate', generateCards);
-router.post('/verify', verifyCard);
-router.post('/custom-generate', generateCustomCards);
-
-module.exports = router;
